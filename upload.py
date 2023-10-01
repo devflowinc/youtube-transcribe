6d0274cf6bdd847a87cd1897111d2f3fb189fd01
kokobhara <146670393+kokobhara@users.noreply.github.com>
2023-10-02 02:31:49 +0530

    Refactor the upload script

--- upload.py
+++ upload.py
@@ -6,16 +6,30 @@ CHANNEL_NAME = "lexfridman"
 r = redis.Redis(host="localhost", port=6379, decode_responses=True)
 
 playlist_id = requests.get(
-    f"https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&forUsername={CHANNEL_NAME}&key={API_KEY}"
-).json()["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
-
-# Make a request to the YouTube Data API
-response = requests.get(
-    f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails%2Cstatus&maxResults=100&playlistId={playlist_id}&key={API_KEY}"
+    "https://youtube.googleapis.com/youtube/v3/channels",
+    params={
+        "part": "snippet,contentDetails,statistics",
+        "forUsername": CHANNEL_NAME,
+        "key": API_KEY,
+    },
 )
-length = 0
+playlist_id.raise_for_status()
+playlist_id = playlist_id.json()["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
+
 total = 0
-if response.status_code == 200:
+params = {
+    "part": "snippet,contentDetails,status",
+    "maxResults": 100,
+    "playlistId": playlist_id,
+    "key": API_KEY,
+}
+while True:
+    response = requests.get("https://youtube.googleapis.com/youtube/v3/playlistItems", params=params)
+    try:
+        response.raise_for_status()
+    except requests.exceptions.RequestException:
+        print(f"Error: {response.text}")
+        continue
     data = response.json()
     videos = data.get("items", [])
     for video in videos:
@@ -24,23 +38,7 @@ if response.status_code == 200:
             f'Upload: https://www.youtube.com/watch?v={video["contentDetails"]["videoId"]}',
             video["snippet"]["title"],
         )
-        print(total)
-
-
-while "nextPageToken" in response.json():
-    nextPageToken = response.json()["nextPageToken"]
-    response = requests.get(
-        f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails%2Cstatus&maxResults=100&pageToken={nextPageToken}&playlistId={playlist_id}&key={API_KEY}"
-    )
-    if response.status_code == 200:
-        data = response.json()
-        videos = data.get("items", [])
-        for video in videos:
-            total += 1
-            r.set(
-                f'Upload: https://www.youtube.com/watch?v={video["contentDetails"]["videoId"]}',
-                video["snippet"]["title"],
-            )
-            print(total)
+    if "nextPageToken" in data:
+        params["pageToken"] = data["nextPageToken"]
     else:
-        print(f"Error: {response.text}")
+        break
